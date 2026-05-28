3a48193cd3cc0500ee59f87f1078b4b671c608d6
LeeKiWoong <littlemanner1@gmail.com>
2026-05-28 10:58:46 +0900

    [RIM Project] UI 기본적인 틀 BaseUI 추가

---
+++ Assets/Script/Base/BaseUI.cs
@@ -0,0 +1,33 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class BaseUI : MonoBehaviour
+{
+    protected bool isAssetSyncLoad = false;
+    protected bool isAssetAsyncLoad = false;
+
+    protected void Log(string text)
+    {
+        Debug.Log($"{this.gameObject} : " + text);
+    }
+
+    protected void LogWarning(string text)
+    {
+        Debug.LogWarning($"{this.gameObject} : " + text);
+    }
+
+    protected void LogError(string text)
+    {
+        Debug.LogError($"{this.gameObject} : " + text);
+    }
+
+    protected virtual async UniTask LoadAssetAsync()
+    {
+        isAssetAsyncLoad = true;
+    }
+
+    protected virtual void LoadAssetSync()
+    {
+        isAssetSyncLoad = true;
+    }
+}
