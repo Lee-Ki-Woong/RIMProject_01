1e22dd5fbdd6612cbc5bb68d7074baee2889e7aa
LeeKiWoong <littlemanner1@gmail.com>
2026-05-28 15:53:48 +0900

    [RIM Project] ResourceManager로부터 prefab을 동기로 받아 던져주는 LoadUtil 추가

---
+++ Assets/Script/Util/LoadUtil.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public static class LoadUtil
+{
+    public static GameObject LoadPrefab(string address)
+    {
+        GameObject prefab = ResourceManager.Instance.LoadAssetSync<GameObject>(address);
+
+        if (prefab == null)
+        {
+            return null;
+        }
+
+        return prefab;
+    }
+}
