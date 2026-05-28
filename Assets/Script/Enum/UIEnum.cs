90afa2bfab40ffb8bf1908357184372b743860a5
LeeKiWoong <littlemanner1@gmail.com>
2026-05-28 14:24:48 +0900

    [RIM Project] UIManager 추가를 위한 UIEnum 추가

---
+++ Assets/Script/Enum/UIEnum.cs
@@ -0,0 +1,15 @@
+﻿public enum UIType : byte
+{
+    None,
+    BackGround,
+    MainMenu,
+    CharacterCollection,
+}
+
+public enum  UIRootType : byte
+{
+    Background,
+    Main,
+    Popup,
+    Loading,
+}
