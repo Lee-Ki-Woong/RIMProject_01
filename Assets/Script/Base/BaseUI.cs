9f27bff0e6f539e886c9155266ac8d6782b53943
LeeKiWoong <littlemanner1@gmail.com>
2026-05-28 15:56:08 +0900

    [RIM Project] BaseUI에 this.GameObject를 켜고 끄는 메서드 추가

--- Assets/Script/Base/BaseUI.cs
+++ Assets/Script/Base/BaseUI.cs
@@ -6,6 +6,17 @@ public class BaseUI : MonoBehaviour
     public bool isAssetSyncLoad { get; private set; } = false;
     public bool isAssetAsyncLoad { get; private set; } = false;
 
+    public virtual void ActiveTrue()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public virtual void ActiveFalse()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+
     protected virtual async UniTask LoadAssetAsync()
     {
         isAssetAsyncLoad = true;
