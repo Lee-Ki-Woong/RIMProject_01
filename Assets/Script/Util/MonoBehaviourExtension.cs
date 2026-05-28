b5eca2ee128bc2af26250f4b6986a99f61d0e251
LeeKiWoong <littlemanner1@gmail.com>
2026-05-28 18:40:29 +0900

    [RIM Project] 모노비에이비어 확장 매서드 추가(3)
    자신의 게임 오브젝트를 활성화/비활성화 하는 로직 추가
    
    파라미터로 받은 컴포넌트가 null일 경우 경고로그를 띄우고 GetComponent<T>로 할당을 시도해보고,
    그래도 null일 경우 에러로그를 띄우고, 게임 오브젝트를 비활성화 한 후 false를 반환하며,
    컴포넌트의 할당에 성공하거나, 컴포넌트가 있을 경우 true를 반환하는 로직 추가

--- Assets/Script/Util/MonoBehaviourExtension.cs
+++ Assets/Script/Util/MonoBehaviourExtension.cs
@@ -17,6 +17,7 @@ public static class MonoBehaviourExtension
         Debug.LogError($"{monoBehaviour.gameObject.name} : " + text);
     }
 
+
     public static GameObject InstantiateGameObject(this MonoBehaviour monoBehaviour, GameObject gameObject, Transform parent)
     {
         if(gameObject == null)
@@ -34,5 +35,35 @@ public static class MonoBehaviourExtension
         GameObject instantiateGameObject = Object.Instantiate(gameObject, parent);
 
         return instantiateGameObject;
+    }
+
+
+    public static void ActiveTrue(this MonoBehaviour monoBehaviour)
+    {
+        monoBehaviour.gameObject.SetActive(true);
+    }
+
+    public static void ActiveFalse(this MonoBehaviour monoBehaviour)
+    {
+        monoBehaviour.gameObject.SetActive(false);
+    }
+
+
+    public static bool ComponentChecking<T>(this MonoBehaviour monoBehaviour, ref T component) where T : Component
+    {
+        if (component == null)
+        {
+            if (component = monoBehaviour.gameObject.GetComponent<T>())
+            {
+                monoBehaviour.LogWarning($"{typeof(T).Name}가 인스펙터에서 할당되지 않아 임시로 GetComponent를 사용하여 할당하였습니다!!");
+                return true;
+            }
+
+            monoBehaviour.LogError($"{typeof(T).Name}가 인스펙터에서 할당되지 않았습니다!! 확인해주세요!! 임시로 이 오브젝트를 비활성화합니다!!");
+            monoBehaviour.ActiveFalse();
+            return false;
+        }
+
+        return true;
     }
 }
