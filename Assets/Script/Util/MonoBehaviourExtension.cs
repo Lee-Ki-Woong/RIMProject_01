7f04443448aa116f44610804ab781cec99b7faca
LeeKiWoong <littlemanner1@gmail.com>
2026-05-28 15:55:16 +0900

    [RIM Project] 모노비에이비어 확장 매서드 추가(2)
    게입 오브젝트를 생성하고, 실패했을 시 오류를 뱉는 메서드 추가

--- Assets/Script/Util/MonoBehaviourExtension.cs
+++ Assets/Script/Util/MonoBehaviourExtension.cs
@@ -16,4 +16,23 @@ public static class MonoBehaviourExtension
     {
         Debug.LogError($"{monoBehaviour.gameObject.name} : " + text);
     }
+
+    public static GameObject InstantiateGameObject(this MonoBehaviour monoBehaviour, GameObject gameObject, Transform parent)
+    {
+        if(gameObject == null)
+        {
+            monoBehaviour.LogError("게임 오브젝트 생성에 실패하였습니다!! 게임 오브젝트가 null입니다!!");
+            return null;
+        }
+
+        if (parent == null)
+        {
+            monoBehaviour.LogError($"게임 오브젝트의 생성에 실패하였습니다!! 부모 트랜스폼이 null입니다!!");
+            return null;
+        }
+
+        GameObject instantiateGameObject = Object.Instantiate(gameObject, parent);
+
+        return instantiateGameObject;
+    }
 }
