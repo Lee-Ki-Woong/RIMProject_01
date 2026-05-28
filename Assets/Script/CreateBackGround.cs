005355ea6dda494120a06ea0abd7efe759f95feb
LeeKiWoong <littlemanner1@gmail.com>
2026-05-28 14:08:55 +0900

    [RIM Project] ResourceManager를 통해 동기로 프리팹 로드가 되는 걸 확인

---
+++ Assets/Script/CreateBackGround.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CreateBackGround : MonoBehaviour
+{
+    private void Start()
+    {
+        CreateBackGroundThisTransform();
+    }
+
+    private void CreateBackGroundThisTransform()
+    {
+        GameObject backGround = ResourceManager.Instance.LoadAssetSync<GameObject>("UI/BackGround");
+
+        Instantiate(backGround, this.transform);
+    }
+   
+
+}
