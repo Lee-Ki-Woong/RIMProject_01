aa8daca5b37bb635fcc5f3243d91301d005a5da7
LeeKiWoong <littlemanner1@gmail.com>
2026-05-28 13:44:22 +0900

    [RIM Project] 모노비헤이비어 확장메서드 추가
    Debug.Log 및 LogWarning, LogError를 편하게 사용하기 위한 확장 메서드 추가

--- Assets/Script/Base/BaseUI.cs
+++ Assets/Script/Base/BaseUI.cs
@@ -6,21 +6,6 @@ public class BaseUI : MonoBehaviour
     protected bool isAssetSyncLoad = false;
     protected bool isAssetAsyncLoad = false;
 
-    protected void Log(string text)
-    {
-        Debug.Log($"{this.gameObject.name} : " + text);
-    }
-
-    protected void LogWarning(string text)
-    {
-        Debug.LogWarning($"{this.gameObject.name} : " + text);
-    }
-
-    protected void LogError(string text)
-    {
-        Debug.LogError($"{this.gameObject.name} : " + text);
-    }
-
     protected virtual async UniTask LoadAssetAsync()
     {
         isAssetAsyncLoad = true;
---
+++ Assets/Script/Util/MonoBehaviourExtension.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class MonoBehaviourExtension
+{
+    public static void Log(this MonoBehaviour monoBehaviour, string text)
+    {
+        Debug.Log($"{monoBehaviour.gameObject.name} : " + text);
+    }
+
+    public static void LogWarning(this MonoBehaviour monoBehaviour, string text)
+    {
+        Debug.LogWarning($"{monoBehaviour.gameObject.name} : " + text);
+    }
+
+    public static void LogError(this MonoBehaviour monoBehaviour, string text)
+    {
+        Debug.LogError($"{monoBehaviour.gameObject.name} : " + text);
+    }
+}
