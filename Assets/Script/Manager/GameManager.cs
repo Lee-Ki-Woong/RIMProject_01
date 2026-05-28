d35fd9fce8f2ef4aadeab306b95a323b13434eed
LeeKiWoong <littlemanner1@gmail.com>
2026-05-28 17:37:03 +0900

    [RIM Project] GameManager 추가
    GameManager가 Manager를 필드에 캐싱하여 관리하도록 설정
    
    GameManager가 Awake할 때 Manager가 제대로 등록되어 있는지 및 자신을 파괴못하게 설정
    
    GameManager가 Start 할 때 UIManager에게 초기 UI를 생성하도록 설정

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class GameManager : BaseManager<GameManager>
+{
+    [SerializeField] private UIManager UI;
+    [SerializeField] private ResourceManager Resource;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        AwakeSetting();
+    }
+
+    private void AwakeSetting()
+    {
+        ManagerCheck();
+        DontDestroyGameManager();
+    }
+
+    private void ManagerCheck()
+    {
+        if (UI == null)
+        {
+            this.LogError("UIManager가 할당되지 않았습니다!!");
+        }
+
+        if (Resource == null)
+        {
+            this.LogError("ResourceManager가 할당되지 않았습니다!!");
+        }
+    }
+
+    private void DontDestroyGameManager()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        StartSetting();
+    }
+    
+    private void StartSetting()
+    {
+        OpenFirstUI();
+    }
+
+    private void OpenFirstUI()
+    {
+        if(UI == null)
+        {
+            return;
+        }
+
+        UI.OpenUI<MainMenu>(UIType.MainMenu);
+    }
+}
