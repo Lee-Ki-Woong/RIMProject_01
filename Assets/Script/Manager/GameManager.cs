e8e7f4555c1f9031a7ce1b7dae52389744bcc50a
LeeKiWoong <littlemanner1@gmail.com>
2026-06-01 12:35:47 +0900

    [RIM Project] GameManager가 게임이 시작할 때 Manager를 동적 생성하도록 수정

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -1,10 +1,15 @@
-﻿using Cysharp.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : BaseManager<GameManager>
 {
-    [SerializeField] private UIManager UI;
-    [SerializeField] private ResourceManager Resource;
+    [SerializeField] GameObject Prefab_UIManager;
+    [SerializeField] GameObject Prefab_ResourceManager;
+    [SerializeField] GameObject Prefab_GameDataManager;
+
+
+    private UIManager UI;
+    private ResourceManager Resource;
+    private GameDataManager GameData;
 
     protected override void Awake()
     {
@@ -20,14 +25,19 @@ public class GameManager : BaseManager<GameManager>
 
     private void ManagerCheck()
     {
-        if (UI == null)
+        if (Prefab_UIManager == null)
         {
             this.LogError("UIManager가 할당되지 않았습니다!!");
         }
 
-        if (Resource == null)
+        if (Prefab_ResourceManager == null)
         {
             this.LogError("ResourceManager가 할당되지 않았습니다!!");
+        }
+
+        if (Prefab_GameDataManager == null)
+        {
+            this.LogError("GameDataManager가 할당되지 않았습니다!!");
         }
     }
 
@@ -43,5 +53,40 @@ public class GameManager : BaseManager<GameManager>
 
     private void StartSetting()
     {
+        if (this.TryInstantiate(Prefab_UIManager, this.transform, out GameObject uiManagerInstance))
+        {
+            if (uiManagerInstance.TryGetComponent(out UIManager uiManager))
+            {
+                UI = uiManager;
+            }
+            else
+            {
+                this.LogError("UIManager Prefab에 UIManager 컴포넌트가 없습니다!!");
+            }
+        }
+
+        if (this.TryInstantiate(Prefab_ResourceManager, this.transform, out GameObject resourceManagerInstance))
+        {
+            if (resourceManagerInstance.TryGetComponent(out ResourceManager resourceManager))
+            {
+                Resource = resourceManager;
+            }
+            else
+            {
+                this.LogError("ResourceManager Prefab에 ResourceManager 컴포넌트가 없습니다!!");
+            }
+        }
+
+        if (this.TryInstantiate(Prefab_GameDataManager, this.transform, out GameObject gameDataManagerInstance))
+        {
+            if (gameDataManagerInstance.TryGetComponent(out GameDataManager gameDataManager))
+            {
+                GameData = gameDataManager;
+            }
+            else
+            {
+                this.LogError("GameDataManager Prefab에 GameDataManager 컴포넌트가 없습니다!!");
+            }
+        }
     }
 }
