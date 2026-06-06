868a9116492e8aeb024b625099f7efa71e4b1235
LeeKiWoong <littlemanner1@gmail.com>
2026-06-07 02:17:30 +0900

    [RIM Project] GameObjectManager에서 Party를 생성 후 캐싱 및 제거하는 로직 추가

--- Assets/Script/Manager/GameObjectManager.cs
+++ Assets/Script/Manager/GameObjectManager.cs
@@ -1,10 +1,10 @@
 ﻿using Cysharp.Threading.Tasks;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class GameObjectManager : BaseManager<GameObjectManager>
 {
-
-
+    private Party m_party;
 
     protected override void Awake()
     {
@@ -12,19 +12,38 @@ public class GameObjectManager : BaseManager<GameObjectManager>
 
     }
 
-    private void AwakeSetting()
+    public void SetParty(List<CharacterData> partyData)
     {
+        if (m_party == null)
+        {
+            CreateParty();
+        }
 
+        m_party.InitPlayerData(partyData);
     }
 
-    private async UniTask LoadEnemyData()
+    public void DestroyParty()
     {
-        foreach(KeyValuePair<string, EnemyData> keyValuePair in GameDataManager.Instance.EnemyDataList)
+        if (m_party != null)
         {
-            return;
+            Destroy(m_party.gameObject);
         }
     }
 
+    private void CreateParty()
+    {
+        if (this.TryInstantiate(LoadUtil.Sync.LoadPrefab(AddressUtil.Sync.Prefab.Party), this.transform, out GameObject partyInstance) == false)
+        {
+            this.LogError("Party 프리팹을 생성할 수 없습니다!!");
+            return;
+        }
 
+        if (partyInstance.TryGetComponent(out Party partyScript) == false)
+        {
+            this.LogError("Party 프리팹에서 Party 스크립트를 찾을 수 없었습니다!!");
+            return;
+        }
 
+        m_party = partyScript;
+    }
 }
