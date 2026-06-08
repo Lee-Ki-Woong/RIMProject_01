d379e20f73cb49cf81926283eaf947d34ae8295e
LeeKiWoong <littlemanner1@gmail.com>
2026-06-09 03:53:58 +0900

    [RIM Project] 구조잡기 : MonoBehaviour를 상속받은 BaseMonoManager와 순수 C# 클래스인 BaseManager로 BaseManager를 분리
    추가로 순수 C# 클래스인 BaseManager는 ComponentExtension의 확장 메서드를 사용할 수 없으니 내부에 Log, LogWarning, LogError를 추가함으로써 편의성도 생각하였음

--- Assets/Script/Base/BaseManager.cs
+++ Assets/Script/Base/BaseManager.cs
@@ -1,23 +1,30 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 
-public class BaseManager<T> : MonoBehaviour where T : MonoBehaviour
+public class BaseManager<T> where T : class
 {
-    public static T Instance { get; private set; }
+    public static T Instance { get; protected set; }
 
-    protected virtual void Awake()
-    {
-        SingleTon();
-    }
-
-    private void SingleTon()
+    protected BaseManager()
     {
         if (Instance == null)
         {
             Instance = this as T;
         }
-        else
-        {
-            Destroy(this);
-        }
+    }
+
+    public void Log(string text)
+    {
+        Debug.Log($"{this} : " + text);
+    }
+
+    public void LogWarning(string text)
+    {
+        Debug.LogWarning($"{this} : " + text);
+    }
+
+    public void LogError(string text)
+    {
+        Debug.LogError($"{this} : " + text);
     }
 }
