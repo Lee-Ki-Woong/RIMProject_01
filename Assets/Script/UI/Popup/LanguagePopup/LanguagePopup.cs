bff4df91bfc44a7259d1433446b5af9f3767d2b9
LeeKiWoong <littlemanner1@gmail.com>
2026-06-10 11:18:48 +0900

    [RIMProject] 언어를 변경할 수 있는 LanguagePopup추가

---
+++ Assets/Script/UI/Popup/LanguagePopup/LanguagePopup.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class LanguagePopup : BaseUI
+{
+    [SerializeField] private Image Image_Background;
+
+    [Serializable]
+    private class MenuButton
+    {
+        public Button Button_This;
+        public Image Image_Button;
+        public TMP_Text Text_Button;
+    }
+
+    [SerializeField] private MenuButton Button_Korean;
+    [SerializeField] private MenuButton Button_English;
+    [SerializeField] private MenuButton Button_Exit;
+
+    public event Action OnChangeLanguageKorean;
+    public event Action OnChangeLanguageEnglish;
+    public event Action OnExit;
+
+    private void OnEnable()
+    {
+        BindButtonEvents();
+    }
+
+    private void BindButtonEvents()
+    {
+        Button_Korean.Button_This.onClick.AddListener(InvokeChangeLanguageKorean);
+        Button_English.Button_This.onClick.AddListener(InvokeChangeLanguageEnglish);
+        Button_Exit.Button_This.onClick.AddListener(InvokeExit);
+    }
+
+    private void OnDisable()
+    {
+        UnBindButtonEvents();
+    }
+
+    private void UnBindButtonEvents()
+    {
+        Button_Korean.Button_This.onClick.RemoveAllListeners();
+        Button_English.Button_This.onClick.RemoveAllListeners();
+        Button_Exit.Button_This.onClick.RemoveAllListeners();
+    }
+
+    public void SetAsset(Sprite sprite_background, Sprite sprite_menuButtons, TMP_FontAsset fontAsset_baseFont)
+    {
+        Image_Background.sprite = sprite_background;
+
+        Button_Korean.Image_Button.sprite = sprite_menuButtons;
+        Button_Korean.Text_Button.font = fontAsset_baseFont;
+
+        Button_English.Image_Button.sprite = sprite_menuButtons;
+        Button_English.Text_Button.font = fontAsset_baseFont;
+        
+        Button_Exit.Image_Button.sprite = sprite_menuButtons;
+        Button_Exit.Text_Button.font = fontAsset_baseFont;
+    }
+
+    public void SetData(string koreanButtonText, string englishButtonText, string exitButtonText)
+    {
+        Button_Korean.Text_Button.text = koreanButtonText;
+        Button_English.Text_Button.text = englishButtonText;
+        Button_Exit.Text_Button.text = exitButtonText;
+    }
+
+    private void InvokeChangeLanguageKorean()
+    {
+        OnChangeLanguageKorean?.Invoke();
+    }
+
+    private void InvokeChangeLanguageEnglish()
+    {
+        OnChangeLanguageEnglish?.Invoke();
+    }
+
+    private void InvokeExit()
+    {
+        OnExit?.Invoke();
+    }
+
+}
---
+++ Assets/Script/UI/Popup/LanguagePopup/LanguagePopupPresenter.cs
@@ -0,0 +1,116 @@
+﻿using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+public class LanguagePopupPresenter : BasePresenterTwo
+{
+    private LanguagePopup m_lnaguagePopupUI;
+
+
+    private Sprite m_sprite_background;
+    private Sprite m_sprite_menuButtons;
+
+    private TMP_FontAsset m_fontAsset_baseFont;
+
+    private string m_koreanButtonText;
+    private string m_englishButtonText;
+    private string m_exitButtonText;
+
+    public void InitLanguagePopup(LanguagePopup languagePopup)
+    {
+        if (m_lnaguagePopupUI == null)
+        {
+            m_lnaguagePopupUI = languagePopup;
+        }
+
+        if(m_isAssetLoad == false)
+        {
+            m_lnaguagePopupUI.ActiveFalse();
+            LoadAndSetAssetAsync().Forget();
+        }
+        else
+        {
+            m_lnaguagePopupUI.ActiveTrue();
+        }
+
+        SubscribeEvents();
+        SubscribeLanguageEvent();
+        LoadUIData();
+    }
+
+    protected override void SubscribeEvents()
+    {
+        m_lnaguagePopupUI.OnChangeLanguageKorean += OnClick_ChangeLanguageKoreanButton;
+        m_lnaguagePopupUI.OnChangeLanguageEnglish += OnClick_ChangeLanguageEnglishButton;
+        m_lnaguagePopupUI.OnExit += OnClick_ExitButton;
+    }
+
+    private void SubscribeLanguageEvent()
+    {
+        GameManager.Instance.OnLanguageChanged += On_ChangeLanguage;
+    }
+
+    protected override void UnsubscribeEvents()
+    {
+        m_lnaguagePopupUI.OnChangeLanguageKorean -= OnClick_ChangeLanguageKoreanButton;
+        m_lnaguagePopupUI.OnChangeLanguageEnglish -= OnClick_ChangeLanguageEnglishButton;
+        m_lnaguagePopupUI.OnExit -= OnClick_ExitButton;
+    }
+
+    private void UnsubscribeLanguageEvent()
+    {
+        GameManager.Instance.OnLanguageChanged -= On_ChangeLanguage;
+    }
+
+    protected async override UniTask LoadAndSetAssetAsync()
+    {
+        var (sprite_background, sprite_menuButtons, fontAsset_baseFont) = await UniTask.WhenAll
+            (
+            LoadUtil.Async.LoadSpriteAsync(AddressUtil.Async.Sprite.UI.LanguagePopup.Background),
+            LoadUtil.Async.LoadSpriteAsync(AddressUtil.Async.Sprite.UI.LanguagePopup.MenuButtons),
+            LoadUtil.Async.LoadFontAssetAsync(AddressUtil.Async.Font.BaseFont)
+            );
+
+        m_sprite_background = sprite_background;
+        m_sprite_menuButtons = sprite_menuButtons;
+        m_fontAsset_baseFont = fontAsset_baseFont;
+
+        m_lnaguagePopupUI.SetAsset(m_sprite_background, m_sprite_menuButtons, m_fontAsset_baseFont);
+
+        m_isAssetLoad = true;
+        m_lnaguagePopupUI.ActiveTrue();
+    }
+
+    protected void LoadUIData()
+    {
+        UIDataManager.Instance.LanguagePopupList.TryGetValue(DataKeyUtil.UI.LanguagePopup, out LanguagePopupData languagePopupData);
+
+        m_koreanButtonText = languagePopupData.KoreanButton;
+        m_englishButtonText = languagePopupData.EnglishButton;
+        m_exitButtonText = languagePopupData.ExitButton;
+
+        m_lnaguagePopupUI.SetData(m_koreanButtonText, m_englishButtonText, m_exitButtonText);
+    }
+
+    private void OnClick_ChangeLanguageKoreanButton()
+    {
+        GameManager.Instance.ChangeLanguage(Language.Korean);
+    }
+
+    private void OnClick_ChangeLanguageEnglishButton()
+    {
+        GameManager.Instance.ChangeLanguage(Language.English);
+    }
+
+    private void OnClick_ExitButton()
+    {
+        UnsubscribeEvents();
+        UnsubscribeLanguageEvent();
+        UIManager.Instance.CloseUI(UIType.LanguagePopup);
+    }
+
+    private void On_ChangeLanguage()
+    {
+        LoadUIData();
+    }
+}
